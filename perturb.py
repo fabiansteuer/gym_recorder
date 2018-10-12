4f5e086382068e085d18b1388789f960ab87e6b9
fasteuer <faebse@posteo.de>
2018-10-12 11:55:51 +0100

    Add perturb.py

---
+++ perturb.py
@@ -0,0 +1,57 @@
+'''Perturb tensors to see how this impacts a model's predictions.'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.ndimage.filters import gaussian_filter
+
+
+def get_mask(center, shape, radius=1, sigma=3):
+    '''Get a 2D Gaussian mask around center in a tensor of size shape.'''
+    y,x = np.ogrid[-center[0]:shape[0]-center[0], -center[1]:shape[1]-center[1]]
+    keep = x*x + y*y <= radius
+    mask = np.zeros(shape)
+    mask[keep] = 1  # keep a circle with radius radius around center
+    mask = gaussian_filter(mask, sigma)  # blur the circle to create a smooth mask
+    return mask / mask.max()
+
+# Pertubation functions
+perturber_mapping = {}
+
+def register_perturber(name):
+    def registerer(func):
+        perturber_mapping[name] = func
+    return registerer
+
+@register_perturber('blurlight')
+def blurlight(image, mask, sigma=4):
+    '''Blur a small region of image.'''
+    return image*(1-mask) + gaussian_filter(image, sigma=16)*mask
+
+
+@register_perturber('searchlight')
+def searchlight(image, mask, sigma=4):
+    '''Blur everything but a small region of image.'''
+    return image*mask + gaussian_filter(image, sigma)*(1-mask)
+
+
+def perturb(tensor, perturber='blurlight', step_size=1):
+    '''Perturb tensor using perturber function with a step size of step_size.'''
+
+    perturber = perturber_mapping[perturber]
+    print(perturber_mapping)
+    
+    shape = tensor.shape
+    assert len(shape) == 4, ('Tensor to perturb should be of shape [batch_size, x, y, n_channels] '
+                             f'but has shape {shape}.')
+    
+    centers = []
+    perturbed = []
+    for i in range(0, shape[1], step_size):
+        for j in range(0, shape[2], step_size):
+            mask = get_mask(center=[i,j], shape=shape[1:3])[:,:,np.newaxis]
+            perturbed.append(perturber(tensor, mask))
+            centers.append((i,j))
+
+    assert perturbed[0].shape == shape, 'Peturbed tensor has a different shape than the original tensor.'
+
+    return perturbed, centers
--- recorder.py
+++ recorder.py
@@ -5,8 +5,8 @@ import logging
 from importlib import reload
 reload(logging)  # necessary for logging in Jupyter notebooks
 logging.basicConfig(format='%(asctime)s %(message)s')
-logger = logging.getLogger()
-logger.setLevel(level=logging.INFO) 
+log = logging.getLogger()
+log.setLevel(level=logging.INFO) 
 
 import time
 import numpy as np
@@ -16,10 +16,10 @@ from .utils import get_activations
 
 class Recorder(object):
     
-    def __init__(self, env, model, operations):
+    def __init__(self, env, act, operations):
         
         self.env = env
-        self.model = model
+        self.act = act
         self.operations = operations
 
         self.frames = []
@@ -27,7 +27,7 @@ class Recorder(object):
         self.actions = []
         self.episode_rewards = []
         
-        self.n_runs = []
+        self.n_episodes = []
         self.n_steps = []
         
         self.activations = {}
@@ -42,18 +42,18 @@ class Recorder(object):
         TODO Think about max_episodes and max_steps order
         '''
         
-        logger.info(f'Start recording for {max_steps} steps or {max_episodes} episodes '
+        log.info(f'Start recording for {max_steps} steps or {max_episodes} episodes '
                     f'with a sample modulo of {sample_modulo}')
 
-        n_run = 0
+        n_episode = 0
         total_steps = 0
         start_time = time.time()
         stop = False
         feed_dict = {}
 
-        while n_run < max_episodes:
+        while n_episode< max_episodes:
 
-            n_run += 1
+            n_episode += 1
             n_step = 0
             ob = self.env.reset()  # LazyFrames object that stores the observation as numpy array
             ob = ob[None]  # extract Numpy array from LazyFrames object
@@ -71,14 +71,14 @@ class Recorder(object):
                 record_step = (n_step % sample_modulo == 0)
                 
                 if record_step:
-                    self.n_runs.append(n_run)
+                    self.n_episodes.append(n_episode)
                     self.n_steps.append(n_step)
                     self.frames.append(self.env.render(mode='rgb_array'))
                     self.observations.append(ob)
 
-                action = self.model.step(ob)
+                action = self.act.step(ob)
                 ob, reward, done, _ = self.env.step(action)
-                ob = ob[None] # extract numpy array from LazyFrames object
+                ob = ob[None] # extract NumPy array from LazyFrames object
                 episode_reward += reward
 
                 if record_step:
@@ -96,18 +96,18 @@ class Recorder(object):
                                 feed_dict = feed_dict))
                     
                 if total_steps % 100 == 0:
-                    logger.info(f'Step {total_steps} in episode {n_run}')
+                    log.info(f'Step {total_steps} in episode {n_episode}')
 
             if stop:
                 break
 
-        logger.info(f'Done recording {total_steps} steps with a sample modulo of {sample_modulo} '
+        log.info(f'Done recording {total_steps} steps with a sample modulo of {sample_modulo} '
                     f'in {round(time.time()-start_time, 1)} seconds')
                     
     def replay(self, start=None, stop=None, step=None, mode='frames'):
 
         def reduce_observations():
-            '''Reduce observation tensors of shape [batch_size, x, y, last_n_frames] to shape [x,y].'''
+            '''Reduce observation tensors of shape [1 , x, y, last_n_frames] to shape [x,y].'''
             # Remove empty dimension and reduce by calculating the mean over the last remaining dimension 
             reduced = [np.add.reduce(np.squeeze(ob), axis=2) / ob.shape[-1] for ob in self.observations]
             return reduced 
@@ -117,6 +117,5 @@ class Recorder(object):
         elif mode == 'observations':
             tape = reduce_observations()
         for image in tape[start:stop+1:step]:
-            print(image.shape)
             plt.imshow(image, cmap='gray')
             plt.show()
